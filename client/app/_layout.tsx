d3e801dde1f64933ba6b1d9e10a984742f0dcd51
Prasanna Achar <prasannaachar64@gmail.com>
2026-03-04 19:55:56 +0530

    layout

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -7,26 +7,18 @@ import Toast from "react-native-toast-message";
 import { ClerkProvider } from '@clerk/clerk-expo'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 
+
 export default function RootLayout() {
-
-  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
-
-
-console.log("MOBILE CLERK KEY:", publishableKey); 
-
   return (
     <GestureHandlerRootView style={{flex: 1}}>
-      <ClerkProvider 
-        publishableKey={publishableKey}
-        tokenCache={tokenCache}
-      >
-        <CartProvider>
-          <WishlistProvider>
-            <Stack screenOptions={{ headerShown: false }} />
-            <Toast />
-          </WishlistProvider>
-        </CartProvider>
-      </ClerkProvider>
+     <ClerkProvider tokenCache={tokenCache}>
+      <CartProvider>
+        <WishlistProvider>
+          <Stack screenOptions={{ headerShown: false }} />
+          <Toast />
+        </WishlistProvider>
+      </CartProvider>
+     </ClerkProvider>
     </GestureHandlerRootView>
   );
 }
